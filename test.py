1567917c91a0c7f55db69e096590f097e7b52f63
ledlong1988 <long.le@usask.ca>
2023-01-01 19:38:43 -0600

    Compare two integer numbers

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
-print('Input a number: ')
-x = int(input())
-print(x)
+x = int(input('Input the first number: '))
+y = int(input('Input the second number: '))
+if x > y:
+	print('x is greater than y')
+elif x == y:
+	print('x is equal to y')
+else:
+	print('x is smaller than y')
+
